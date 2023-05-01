5d89742f4fb1ccb8104ee8a90b221e3a2f35e5b4
bjarkiivarsson <bjarki_ivars2@hotmail.com>
2023-05-01 16:43:24 +0000

    added a pizza model class

--- pizzafront/models.py
+++ pizzafront/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class Pizza(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    PIZZA_CHOICE = [
+        ('S', '9"'),
+        ('M', '12"'),
+        ('L', '16"')
+    ]
+
+    size = models.CharField(max_length=1, choices=PIZZA_CHOICE)
+
+
+
