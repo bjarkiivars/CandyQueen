d2bafc886f3785c14267090d35ac847674ab02a7
bjarkiivarsson <bjarki_ivars2@hotmail.com>
2023-05-02 11:22:11 +0000

    added model class skeleton

--- pizzafront/models.py
+++ pizzafront/models.py
@@ -1,7 +1,46 @@
 from django.db import models
 
 
+# TODO:
+# 1. User tengist Cart 0 to Many
+# 2. Cart hefur FK: user, offer og pizza (allt 0 to Many)
+# 3. Offer tengist Cart, hefur PizzaID FK, (1 to Manu)
+# 4. Pizza hefurr ToppingID FK (2 to Many)
+# 5. Topping
+# 6. MainMenu hefur OfferID Fk, (3 to Many)
+# 7. Menu hefur PizzaID fk
+
 # Create your models here.
+class User(models.Model):
+    pass
+    # userId PK
+    # email
+    # phoneNumber
+    # streetName
+    # houseNumber
+    # city
+    # postalCode
+    # password
+    # img
+
+
+class Cart(models.Model):
+    pass
+    # cartID PK
+    # cartSum
+    # pizzaID FK
+    # offerID Fk
+    # userID Fk
+
+
+class Offer(models.Model):
+    pass
+    # offerID PK
+    # offerName
+    # offerPrice
+    # pizzaID FK
+
+
 class Pizza(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
@@ -15,4 +54,19 @@ class Pizza(models.Model):
     size = models.CharField(max_length=1, choices=PIZZA_CHOICE)
 
 
+class Topping(models.Model):
+    pass
+    # toppingID PK
+    # name
 
+
+class MainMenu(models.Model):
+    pass
+    # mainID PK
+    # offerID FK
+
+
+class Menu(models.Model):
+    pass
+    # menuID pk
+    # pizzaID Fk
