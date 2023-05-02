8cda500f164b79b7f91427441e5bc6ceefa818b4
Bjarki thor ivarsson <Bjarki_ivars2@hotmail.com>
2023-05-02 13:37:09 +0000

    finished model classes

--- pizzafront/models.py
+++ pizzafront/models.py
@@ -1,72 +1,80 @@
 from django.db import models
 
 
-# TODO:
-# 1. User tengist Cart 0 to Many
-# 2. Cart hefur FK: user, offer og pizza (allt 0 to Many)
-# 3. Offer tengist Cart, hefur PizzaID FK, (1 to Manu)
-# 4. Pizza hefurr ToppingID FK (2 to Many)
-# 5. Topping
-# 6. MainMenu hefur OfferID Fk, (3 to Many)
-# 7. Menu hefur PizzaID fk
-
 # Create your models here.
 class User(models.Model):
-    pass
-    # userId PK
+    # name
+    name = models.CharField(max_length=254)
     # email
+    email = models.EmailField(max_length=254)
     # phoneNumber
+    phoneNumber = models.CharField(max_length=254)
     # streetName
+    streetName = models.CharField(max_length=254, null=True)
     # houseNumber
+    houseNumber = models.CharField(max_length=254, null=True)
     # city
+    city = models.CharField(max_length=254, null=True)
     # postalCode
+    postalCode = models.CharField(max_length=254, null=True)
     # password
+    password = models.CharField(max_length=254)
     # img
-
-
-class Cart(models.Model):
-    pass
-    # cartID PK
-    # cartSum
-    # pizzaID FK
-    # offerID Fk
-    # userID Fk
+    img = models.ImageField()
 
 
 class Offer(models.Model):
-    pass
-    # offerID PK
     # offerName
+    offerName = models.CharField(max_length=254)
     # offerPrice
+    offerPrice = models.DecimalField(max_digits=254, decimal_places=2)
     # pizzaID FK
+    pizza = models.ManyToManyField('Pizza')
+
+
+class Topping(models.Model):
+    # name
+    name = models.CharField(max_length=254)
 
 
 class Pizza(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    PIZZA_SMALL = 'S'
+    PIZZA_MEDIUM = 'M'
+    PIZZA_LARGE = 'L'
+
     PIZZA_CHOICE = [
-        ('S', '9"'),
-        ('M', '12"'),
-        ('L', '16"')
+        (PIZZA_SMALL, '9"'),
+        (PIZZA_MEDIUM, '12"'),
+        (PIZZA_LARGE, '16"')
     ]
 
     size = models.CharField(max_length=1, choices=PIZZA_CHOICE)
 
+    topping = models.ManyToManyField(Topping)
 
-class Topping(models.Model):
-    pass
-    # toppingID PK
-    # name
+
+class Cart(models.Model):
+    # cartSum
+    cartSum = models.DecimalField(max_digits=254, decimal_places=2)
+    # createdAt
+    createdAt = models.DateTimeField(auto_now_add=True)
+    # pizzaID
+    pizza = models.ManyToManyField(Pizza)
+    # offerID Fk
+    offer = models.ManyToManyField(Offer)
+    # userID Fk
+    user = models.ForeignKey(User, on_delete=models.RESTRICT)
 
 
 class MainMenu(models.Model):
-    pass
-    # mainID PK
     # offerID FK
+    offer = models.ManyToManyField(Offer)
 
 
 class Menu(models.Model):
-    pass
-    # menuID pk
     # pizzaID Fk
+    pizza = models.ManyToManyField(Pizza)
