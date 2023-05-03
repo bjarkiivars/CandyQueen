87f5c2ff69c1efbcfdf4c36d95c35d0f5cccd553
Bjarki thor ivarsson <Bjarki_ivars2@hotmail.com>
2023-05-03 13:06:13 +0000

    added some stuff to DOM

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,8 @@
-// Retrieve Each pizza image from the Content body to be able to display the pizzas.
+
+// Waits for the DOM to be loaded, helps with accessing DOM elements with query selectors.
 document.addEventListener('DOMContentLoaded', function() {
+    /* ----------------------------------Display Pizza----------------------------------------- */
+    // Retrieve Each pizza image from the Content body to be able to display the pizzas.
     const pizzaList = document.querySelectorAll('.pizza');
     pizzaList.forEach((pizza) => {
         const img = pizza.dataset.img;
@@ -7,4 +10,20 @@ document.addEventListener('DOMContentLoaded', function() {
         const imgElm = pizza.querySelector(`#img-${id}`);
         imgElm.src = `/static/img/${img}`;
     });
+
+    /* ----------------------------------Display Filter----------------------------------------- */
+    // If the endpoint menu is called, we display the filter menu.
+    if(window.location.pathname == '/menu/') {
+        $("#filter").show();
+        $("#menu").show();
+    } else {
+        $("#filter").hide();
+        $("#menu").hide();
+    }
+    if(window.location.pathname == '/offer/') {
+        $("#offer").show();
+    } else {
+        $("#offer").hide();
+    }
 });
+
