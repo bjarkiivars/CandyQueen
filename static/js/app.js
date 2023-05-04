5d66f7c02f845384239deeb843224b0dda669aec
Bjarki thor ivarsson <Bjarki_ivars2@hotmail.com>
2023-05-04 15:08:30 +0000

    added Js from Bjarni

--- static/js/app.js
+++ static/js/app.js
@@ -335,4 +335,24 @@ document.addEventListener('DOMContentLoaded', function() {
     clearOrder();
     clearType();
     clearSearch();
+
+    /* ----------------------------------About us Functionality----------------------------------------- */
+
+    // selecting the elements for which we want to add a tooltip
+    const target = document.getElementById("pizzaAbout");
+    const tooltip = document.getElementById("aboutUsText");
+
+    // change display to 'block' on mouseover
+    target.addEventListener('mouseover', () => {
+      tooltip.style.display = 'block';
+    }, false);
+
+    // change display to 'none' on mouseleave
+    target.addEventListener('mouseleave', () => {
+      tooltip.style.display = 'none';
+    }, false);
+
+
+    /* ----------------------------------View Specific Pizza----------------------------------------- */
+
 });
