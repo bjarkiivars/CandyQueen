f927ea3fc182fc7d4c3fc3801c26d25978b77b99
Bjarki thor ivarsson <Bjarki_ivars2@hotmail.com>
2023-05-10 14:47:51 +0000

    changes to 1 view

--- pizzafront/views.py
+++ pizzafront/views.py
@@ -127,7 +127,7 @@ def addPizzaToOffer(request, user_id, offer_id):
     # iterate the pizza ids in the list
     for pizzaID in pizza_id_list:
         # check if the pizza exists already
-        existing_pizza = OfferPizza.objects.filter(offer=offer).first()
+        existing_pizza = OfferPizza.objects.filter(pizza=pizzaID).first()
         # if the pizza exists
         if existing_pizza:
             # update the quantity
@@ -136,7 +136,7 @@ def addPizzaToOffer(request, user_id, offer_id):
             existing_pizza.save()
         else:
             # if the pizza does not exist, we add the pizza id to the through m2m table
-            OfferPizza.objects.create(offer=offer, pizza_id=pizzaID)
+            OfferPizza.objects.create(offer=offer_id, pizza_id=pizzaID)
 
     # save changes
     offer.save()
